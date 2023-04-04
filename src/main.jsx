e2d71d500d269cc7842bf8f7b1149d728374af8f
Yayi Abiodun <58443752+YAYI68@users.noreply.github.com>
2023-04-04 23:35:18 +0100

    commit

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import {createBrowserRouter, RouterProvider} from "react-router-dom"
 import App from './App'
 import './index.css'
+import { AppProvider } from './context/AppContext'
+
 const router = createBrowserRouter([
   {
     path: "/",
