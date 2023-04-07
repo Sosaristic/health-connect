6f64db9edfb61cda3c231b231d09c3f267a27947
Halezis <halezis007@gmail.com>
2023-04-07 22:11:04 +0100

    commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Hero, Testimonial } from '../components/Home'
+import AboutUs from '../components/Home/AboutUs'
+import Articles from '../components/Home/Articles'
 import Layout from '../components/Layout/Layout'
 import Footer from '../components/Layout/Footer'
 
@@ -7,6 +9,8 @@ export default function Home() {
   return (
     <Layout>
       <Hero />
+      <AboutUs />
+      <Articles />
       <Testimonial />
       <Footer />
     </Layout>
